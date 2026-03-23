79a3f8b2a7cfd2982cc9bde2558c3f841c5fa0e4
Júlio César <juliocesariglesias77@gmail.com>
2026-03-23 20:59:05 -0300

    chore: ajuste de porta do servidor

--- server.js
+++ server.js
@@ -46,6 +46,8 @@ res.json(guests)
 
 })
 
-app.listen(3000, () => {
-console.log("Servidor rodando em http://localhost:3000")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+console.log("Servidor rodando")
 })
